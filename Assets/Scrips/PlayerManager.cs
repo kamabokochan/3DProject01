a6c6def7894df1cfc8c3e45411929a79571d79cd
kamabokochan <h.sasaki.worksfront@gmail.com>
2021-09-05 17:11:15 +0900

    run animation 追加

--- Assets/Scrips/PlayerManager.cs
+++ Assets/Scrips/PlayerManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    Rigidbody rb;
+    Animator animator;
+    float x;
+    float z;
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb = GetComponent<Rigidbody>();
+       animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = transform.position + new Vector3(x, 0, z);
+        transform.LookAt(direction);
+        rb.velocity = new Vector3(x, 0, z)*3f;
+        animator.SetFloat("Speed", rb.velocity.magnitude);
     }
 }
